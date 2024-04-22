2b63ba94ba4bee4c27683363a40b4085e22f7373
Thierno <77080504+thierno953@users.noreply.github.com>
2024-04-22 10:33:26 +0200

    Selenium

---
+++ test.js
@@ -0,0 +1,33 @@
+const { By, Key, Build, Builder } = require("selenium-webdriver");
+require("chromedriver");
+
+async function test_case() {
+  let driver = await new Builder().forBrowser("chrome").build();
+
+  await driver.get("https://www.bing.com/");
+  try {
+    driver = await new Builder().forBrowser("chrome").build();
+
+    await driver.findElement(By.name("q")).sendKeys("Hello, World", Key.RETURN);
+
+    console.log("Opened Google");
+
+    await driver.sleep(10000);
+    const searchInput = await driver.findElement(By.name("q"));
+
+    await driver.quit();
+
+    await searchInput.sendKeys("Hello, World!", Key.RETURN);
+
+    console.log("Text entered into the search input.");
+  } catch (error) {
+    console.error("An error occurred:", error);
+  } finally {
+    if (driver) {
+      await driver.quit();
+      console.log("WebDriver session closed");
+    }
+  }
+}
+
+test_case();
