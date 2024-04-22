2dd652a4635ed19f39bf88e13e2a71de36a360c8
Thierno <77080504+thierno953@users.noreply.github.com>
2024-04-22 19:16:52 +0200

    test

--- test/index.js
+++ test/index.js
@@ -1,26 +1,43 @@
-const { By, Key, Builder, until } = require("selenium-webdriver");
+const { By, Key, Builder } = require("selenium-webdriver");
 require("chromedriver");
+ 
 
 async function test_case() {
-  let driver;
+  let driver = await new Builder().forBrowser("chrome").build();
+
+
+
+  await driver.get("https://www.inami.fgov.be/fr");
   try {
-    driver = await new Builder().forBrowser("chrome").build();
-    await driver.get("https://www.inami.fgov.be/fr");
+    // Initialize the WebDriver session
+   // driver = await new Builder().forBrowser("chrome").build();
 
-    const searchInput = await driver.wait(until.elementLocated(By.name("q")), 10000);
+  await driver.findElement(By.name("q")).sendKeys("Hello, World!", Key.RETURN);
+    // Navigate to Google
 
+    console.log("Opened Google.");
+
+  // Wait for some time before quitting
+  await driver.sleep(10000);
+    // Find the search input element
+    const searchInput = await driver.findElement(By.name("q"));
+
+  // Quit the WebDriver session
+  await driver.quit();
+    // Enter text into the search input
     await searchInput.sendKeys("Hello, World!", Key.RETURN);
 
     console.log("Text entered into the search input.");
 
+    // Wait for some time before quitting
     await driver.sleep(10000);
-
   } catch (error) {
     console.error("An error occurred:", error);
   } finally {
+    // Close the WebDriver session
     if (driver) {
       await driver.quit();
-      console.log("WebDriver session closed");
+      console.log("WebDriver session closed.");
     }
   }
 }
